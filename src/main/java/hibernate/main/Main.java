b8d671689445bf8453e9f0aa8cfc98fe690733c7
Gaurav72100 <gk240662@gmail.com>
2024-11-28 23:50:09 +0530

    Initial commit

---
+++ src/main/java/hibernate/main/Main.java
@@ -0,0 +1,38 @@
+package hibernate.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import hibernate.entity.Employee;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee(9,"Mukesh","male",20000);
+		
+		//Session session = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory().openSession();
+//		Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+//		   SessionFactory sessionfactory= cfg.buildSessionFactory();
+//		  Session session = sessionfactory.openSession();
+//		  
+		
+		
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+		Metadata data = new MetadataSources(ssr).getMetadataBuilder().build();
+		
+		SessionFactory sf = data.buildSessionFactory();
+		Session session = sf.openSession();
+		  Transaction transaction = session.beginTransaction();
+		  session.persist(emp);
+		  transaction.commit();
+		  System.out.println("Executed successfully");
+	}
+}
