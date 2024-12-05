1dfe070e456b8151b8f0d4e85a78fc141632160f
Gaurav72100 <gk240662@gmail.com>
2024-12-05 23:41:29 +0530

    update

--- src/main/java/hibernate/main/Main.java
+++ src/main/java/hibernate/main/Main.java
@@ -31,18 +31,54 @@ public class Main {
 		
 		SessionFactory sf = data.buildSessionFactory();
 		Session session = sf.openSession();
-		  session.beginTransaction().commit();;
-		  System.out.println("Executed successfully");
+		//Transaction txt= session.beginTransaction();
+		session.beginTransaction().commit();
+		  
+		 
 		  
 		  // Hibernate Query Language
 		  
-//		  Query q1= session.createQuery("from Employee");
-//		  List list =  q1.list();
+		  Query q1= session.createQuery("from Employee");
+		  List list =  q1.list();
+		  System.out.println(list);
+		  
+//		  Query q2 = session.createQuery("Select sum(salary) from Empoyee",Employee.class);
+//		  List<Integer> list1 = q2.list();
+//		  System.out.println(list1.get(0));
+		  
+//		  How to record with pagination
+//		  Query q3 = session.createQuery("from Employee");
+//		  q3.setFirstResult(2);
+//		  q3.setMaxResults(3);
+//		  List list = q3.list();
 //		  System.out.println(list);
 		  
-		  Query q2 = session.createQuery("select max(name) from Empoyee",Employee.class);
-		  List<Integer> list1 = q2.list();
-		  System.out.println(list1.get(0));
+		  //How to update Query 
+		  
+//		  Query q4 = session.createQuery("Update Employee set gender =:n where id=:i");
+//		  q4.setParameter("n", "female");
+//		  q4.setParameter("i","105" );
+//		  int status = q4.executeUpdate();// to update query firstly you will to begin transaction then commit at last
+//		  System.out.println(status);
+		  
+//		  txt.commit();
+		
+		// HQL delete QUERY
+		
+//		Query q5 = session.createQuery("delete from Employee where id=101");
+//		q5.executeUpdate();// to delete query firstly you will to begin transaction then commit at last
+//		System.out.println("Executed successfully");
+//		  txt.commit();
+//		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
+		  
 		  
 	}
 }
